6833535091a3db5ebced8b67c91e85451f614caf
Polina <polina.tikunowa@gmail.com>
2024-03-31 21:15:42 +0300

    HW2: urls: added url path

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.get_page, kwargs={'number': 1}),
+    path('pages/<int:number>/', views.get_page, name='page'),
+    path('hot/', views.get_hot, name='hot'),
+    path('tag/<str:tag_id>/', views.get_tag, name='tag'),
+    path('question/<int:question_id>', views.get_question, name='question'),
+    path('login/', views.get_login, name='login'),
+    path('signup/', views.get_signup, name='signup'),
+    path('ask/', views.get_ask, name='ask'),
+    path('settings/', views.get_settings, name='settings'),
+    path('member/<int:user_id>', views.get_member, name='member')
 ]
