e089af6d0255c4b9b859f2a0dbffb62b707efb21
Polina <polina.tikunowa@gmail.com>
2024-03-31 21:31:00 +0300

    HW2: templates: added hot template

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_page, kwargs={'number': 1}),
+    path('', views.get_page, kwargs={'number': 1}, name='main'),
     path('pages/<int:number>/', views.get_page, name='page'),
-    path('hot/', views.get_hot, name='hot'),
+    path('hot/<int:number>/', views.get_hot, name='hot'),
     path('tag/<str:tag_id>/', views.get_tag, name='tag'),
     path('question/<int:question_id>', views.get_question, name='question'),
     path('login/', views.get_login, name='login'),
