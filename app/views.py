622adc1d5ac9ea2b4e27408d029a0e6e8d28c044
Polina <polina.tikunowa@gmail.com>
2024-03-31 21:31:15 +0300

    HW2: views: added views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,84 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+from .default_data import *
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Helloworld!")
+    return render(request, 'index.html', {
+        "user": User(),
+        "tags": tags,
+        "members": members,
+        "questions": QUESTIONS
+    })
+
+
+def get_hot(request, number: int = 1):
+    pages, questions = paginate(QUESTIONS, number)
+    return render(request, "hot.html", {
+        "user": User(),
+        "tags": tags,
+        "members": members,
+        "questions": questions,
+        "pages": pages
+    })
+
+
+def get_question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, "question.html", {
+        "question": item,
+        "user": User(),
+        "tags": tags,
+        "members": members,
+    })
+
+
+def get_tag(request, tag_id: str):
+    return HttpResponse(tag_id)
+
+
+def get_login(request):
+    return render(request, "login.html",
+                  context={
+                      "tags": tags,
+                      "members": members,
+                  })
+
+
+def get_signup(request):
+    return render(request, "signup.html",
+                  context={
+                      "tags": tags,
+                      "members": members,
+                  })
+
+
+def get_ask(request):
+    return render(request, "ask.html",
+                  context={
+                      "tags": tags,
+                      "members": members,
+                  })
+
+
+def get_settings(request):
+    return render(request, "settings.html",
+                  context={
+                      "tags": tags,
+                      "members": members,
+                  })
+
+
+def get_member(request, user_id: int):
+    return HttpResponse(f"{user_id}")
+
+
+def get_page(request, number: int = 1):
+    pages, questions = paginate(QUESTIONS, number)
+    return render(request, "index.html", {
+        "user": User(),
+        "tags": tags,
+        "members": members,
+        "questions": questions,
+        "pages": pages
+    })
