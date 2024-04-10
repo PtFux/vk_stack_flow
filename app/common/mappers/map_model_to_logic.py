e9afae842cf45a9aa6c92a31dbe89bae82998a45
Тикунова Полина <tikunova.polina@proq.software>
2024-04-10 15:33:31 +0300

    HW3: task-views_questions: services: add behavior

---
+++ app/common/mappers/map_model_to_logic.py
@@ -0,0 +1,44 @@
+from app.default_data import Answer
+from app.models import QuestionModel, TagModel, AnswerModel
+from app.services.models.question import Question
+from app.services.models.tag import Tag
+
+
+class MapModelToLogic:
+
+    def map_question_model_to_logic(self, model: QuestionModel) -> Question:
+        question = Question(
+            question_id=int(model.id),
+            title=model.title,
+            text=model.text,
+            rating=0
+        )
+        question.tags = [self.map_tag_model_to_logic(tag)
+                         for tag in model.tags.all()
+                         ]
+        question.answers = [
+            self.map_answer_model_to_logic(answer)
+            for answer in model.answers.all()
+        ]
+        question.n_answers = model.n_answers or len(question.answers)
+        return question
+
+    @staticmethod
+    def map_question_logic_to_map(logic: Question) -> QuestionModel:
+        pass
+
+    @staticmethod
+    def map_tag_model_to_logic(model: TagModel) -> Tag:
+        tag = Tag(
+            tag_id=int(model.id),
+            title=model.title,
+        )
+        return tag
+
+    @staticmethod
+    def map_answer_model_to_logic(model: AnswerModel) -> Answer:
+        answer = Answer(
+            text=model.text,
+            rating=model.rating
+        )
+        return answer
